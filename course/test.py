e1d2ed18539cb4b3dcfd164731fccf15b2f1078c
arseniyyy <arskosh05@mail.ru>
2023-06-03 23:17:20 +0700

    Update 2-variable-assignment-int-float.md

--- course/test.py
+++ course/test.py
@@ -1,6 +1,19 @@
-red_balls = int(input())
-green_balls = int(input())
-blue_balls = int(input())
+import math
 
-print(red_balls + blue_balls + 1)
+# Ввод длин сторон треугольника
+side1 = float(input("Введите длину первой стороны треугольника: "))
+side2 = float(input("Введите длину второй стороны треугольника: "))
+side3 = float(input("Введите длину третьей стороны треугольника: "))
+
+# Вычисление полупериметра треугольника
+semiperimeter = (side1 + side2 + side3) / 2
+
+# Вычисление площади треугольника по формуле Герона
+area = math.sqrt(semiperimeter * (semiperimeter - side1) * (semiperimeter - side2) * (semiperimeter - side3))
+
+# Округление площади до двух знаков после запятой
+rounded_area = int(area)
+
+# Вывод результата
+print("Площадь треугольника:", rounded_area)
 
