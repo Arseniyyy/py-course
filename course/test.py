2dec6d959aec00cc018d82d9948ab98c342328db
arseniyyy <arskosh05@mail.ru>
2023-06-13 01:08:01 +0700

    Update course/4-if-else-statements.md and return course-overview.md

--- course/test.py
+++ course/test.py
@@ -1,5 +1,14 @@
-string = "Hello, Python!"
+print("Добро пожаловать в Волшебный магазин доставки!")
+item_name = input("Введите название волшебного предмета: ")
+weight = float(input("Введите вес предмета (кг): "))
+distance = float(input("Введите расстояние доставки (км): "))
 
-print(string[-1])  # Вернёт "!"
-print(string[-6])  # Вернёт "P"
+if weight < 10 and distance < 100:
+    delivery_cost = 200
+elif weight >= 10 and weight < 50 and distance < 100:
+    delivery_cost = 500
+else:
+    delivery_cost = 200 + 5 * distance
+
+print(f"Стоимость доставки волшебного предмета \"{item_name}\" составляет {delivery_cost} магических монет.")
 
